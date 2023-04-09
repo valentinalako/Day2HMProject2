0764f69aebd5044126016d4ad390c0f985a59294
valen <valen@DESKTOP-057E61O.lan>
2023-04-09 18:09:13 -0400

    new commit

---
+++ src/demo2/Test2.java
@@ -0,0 +1,21 @@
+package demo;
+
+public class Test2 {
+	
+	
+	public static void main (String arg []) {
+		
+		System.out.println("Test 2....");
+	}
+	
+	public void divide (int a , int b) {
+		
+		System.out.println(a/b);
+	}
+public void newTry() {
+		
+		System.out.println("NewTRy");
+	
+}
+
+}
